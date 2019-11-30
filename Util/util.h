debb680845fb2cc2a98d18cc24726c04d45f974d
luisalvarez98 <luisfeal1998@hotmail.com>
2019-11-30 13:53:45 -0600

    [v1.0] Update: Changed the structure of the project, created a util header to store all the functions of the app, also created a console menu

---
+++ Util/util.h
@@ -0,0 +1,59 @@
+/*
+    C++ File used to declare util functions
+*/
+#include <iostream>
+#include "../Headers/Files.h"
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Util{
+    private:
+    ///
+    public:
+
+    int readFile(){
+        Files flag;
+        ifstream file;
+        int del;
+        vector<int> bytes;
+        vector<int> process;
+        string archivo, line;
+        cout << "Dame un archivo" << endl;
+        cin >> archivo;
+        //intenta abrir el archivo si no existe termina el programa
+        if(!(flag.tryOpen(archivo, file))){
+            cout << "El archivo no existe o esta vacio" << endl;    
+            return 0;
+        }
+        //va leyendo los datos del txt; checa que lo primero tenga una P
+        //haciendo referencia al Proceso, la segunda parte hace referencia a los bytes
+        //la tercera parte hace referencia al proceso y los mete a cada uno en vectores separados
+        // *hace falta realizar verificacion de que lo que se mete al vector es realmente un numero entero
+        // *para prevenir fallas
+        while(getline(file, line) ) {
+            if(line.find("P") != std::string::npos) {
+                del = line.find("P");
+            }
+            line.erase(0,del+2);
+            bytes.push_back(stoi(line));
+            if(line.find(" ") != std::string::npos) {
+                del = line.find(" ");
+            }
+            line.erase(0, del+1);
+            process.push_back(stoi(line));
+        }
+        // For testing purposes
+        // imprime los bytes
+        cout << "bytes" << endl;
+        for(auto i : bytes) {
+            cout << i << endl;
+        }
+        // imprime los procesos
+        cout << "process" << endl;
+        for(auto i : process) {
+            cout << i << endl;
+        }
+    };
+};
--- main.cpp
+++ main.cpp
@@ -1,51 +1,43 @@
     #include <iostream>
-    #include "Files.h"
+    #include "Headers/Files.h"
     #include <fstream>
     #include <vector>
     #include <string>
+    #include "Util/util.h"
+
     using namespace std;
     int main() 
     {
-        Files flag;
-        ifstream file;
-        int del;
-        vector<int> bytes;
-        vector<int> process;
-        string archivo, line;
-        cout << "Dame un archivo" << endl;
-        cin >> archivo;
-        //intenta abrir el archivo si no existe termina el programa
-        if(!(flag.tryOpen(archivo, file))){
-            cout << "El archivo no existe o esta vacio" << endl;
-            return 0;
-        }
-        //va leyendo los datos del txt; checa que lo primero tenga una P
-        //haciendo referencia al Proceso, la segunda parte hace referencia a los bytes
-        //la tercera parte hace referencia al proceso y los mete a cada uno en vectores separados
-        // *hace falta realizar verificacion de que lo que se mete al vector es realmente un numero entero
-        // *para prevenir fallas
-        while(getline(file, line) ) {
-            if(line.find("P") != std::string::npos) {
-                del = line.find("P");
-            }
-            line.erase(0,del+2);
-            bytes.push_back(stoi(line));
-            if(line.find(" ") != std::string::npos) {
-                del = line.find(" ");
-            }
-            line.erase(0, del+1);
-            process.push_back(stoi(line));
-        }
-        // For testing purposes
-        // imprime los bytes
-        cout << "bytes" << endl;
-        for(auto i : bytes) {
-            cout << i << endl;
-        }
-        // imprime los procesos
-        cout << "process" << endl;
-        for(auto i : process) {
-            cout << i << endl;
-        }
+       //Declaration of util Class to use the util functions 
+       Util util;
+       //Menu Init
+       int option;
+       bool isTerminated = false;
+       //Runs the menu until the user types any option 
+       while(!isTerminated){
+           cout << "Please select an option of the menu: " << endl;
+           cout << "[1] FIFO" << endl;
+           cout << "[2] LRU" << endl;
+           cout << "[3] FIFO and LRU algorithms" << endl;
+           cout << "[4] Exit" << endl;
+
+           cin >> option;
+           switch(option){
+               case 1:
+                //runFifo();
+               util.readFile();
+               break;   
+               case 2:
+               //runLRU();
+               break;   
+               case 3:
+               //runBoth();
+               //run
+               break;              
+               case 4:
+                isTerminated = true;
+                break;
+           }
+       }
         return 0;
     }
